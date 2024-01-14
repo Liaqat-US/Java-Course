ac290ceb75afd13131cb331ed34b571a3583f6d9
liaqat-programmer <liaqat122012@gmail.com>
2024-01-14 15:53:41 +0500

    For Loop, While Loop And Do-While Loop

---
+++ Loops.java
@@ -0,0 +1,23 @@
+
+public class Loops {
+    public static void main(String[] args) {
+        //for loop
+        for(int i=0; i<10; i++){
+            System.out.println(i);
+        }
+
+        //while loop
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("Pakistan Zindabad!");
+            j++;
+        }
+
+        //do-while loop
+        int k = 1;
+        do {
+            System.out.println("Hello World!");
+            k++;
+        } while (k <= 5);
+    }
+}
