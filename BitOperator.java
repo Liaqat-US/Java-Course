6b8997890ae6cd9c00c28f41f8ebbc968408080f
liaqat-programmer <liaqat122012@gmail.com>
2024-01-17 21:33:24 +0500

    Bitwise Operators

---
+++ BitOperator.java
@@ -0,0 +1,24 @@
+/**
+ * BitOperator
+ */
+public class BitOperator {
+
+    public static void main(String[] args) {
+        //Bit Operators: &, |, ~, ^
+
+        //Bit &: return true if both are 1 otherwise return false
+        // int BitOpt = 5 & 9;
+
+        // Bit |: return true if only 1 otherwise return false
+        // int BitOpt = 5 | 9;
+
+        // Bit ^: return true if both are different otherwise return false
+        // int BitOpt = 5 ^ 9;
+
+        // Bit ~: convert 1 into 0 and 0 into 0
+        int BitOpt = ~(5);
+
+        System.out.print(BitOpt);
+
+    }
+}
