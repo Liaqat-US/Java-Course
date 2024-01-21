ae779df385642ff60c317b8f88059691ec8c8865
liaqat-programmer <liaqat122012@gmail.com>
2024-01-21 21:52:02 +0500

    Word First and Last Comes

---
+++ Recursion/WordCount.java
@@ -0,0 +1,36 @@
+/**
+ * WordCount
+ */
+public class WordCount {
+
+    public static int First = -1;
+    public static int Last = -1;
+
+    public static void WordComes(String word, char x, int idx){
+        if(word.length() == idx){
+            System.out.println("First: "+First);
+            System.out.println("Last: "+Last);
+            System.out.println(idx);
+            return;
+        }
+
+        char curr = word.charAt(idx);
+
+        if(curr == x){
+            if(First == -1){
+                First = idx;
+            }else{
+                Last = idx;
+            }
+        }
+
+        WordComes(word, x, idx + 1);
+
+    }
+
+    public static void main(String[] args) {
+        String word = "adcadaia";
+
+        WordComes(word, 'a', 0);
+    }
+}
