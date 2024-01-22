ef028aca2a723195422af58e6be9cbd59ec2b58a
liaqat-programmer <liaqat122012@gmail.com>
2024-01-22 21:10:45 +0500

    Unique Subsequnce String

---
+++ Recursion/UniqueSubsequnce.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+/**
+ * UniqueSubsequnce
+ */
+public class UniqueSubsequnce {
+
+    public static void SubSequnce(String str, int idx, String newString, HashSet<String> set ){
+        if(idx == str.length()){
+            if(set.contains(newString)){
+                return;
+            }else{
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
+        }
+
+        char curr = str.charAt(idx);
+
+        //to be
+        SubSequnce(str, idx + 1, newString+curr, set);
+
+        //not to be
+        SubSequnce(str, idx + 1, newString, set);
+    }
+
+    public static void main(String[] args) {
+        String str = "aaa";
+
+        HashSet<String> set = new HashSet<>();
+
+        SubSequnce(str, 0, "", set);
+    }
+}
