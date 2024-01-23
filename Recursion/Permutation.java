61b77cc7a67a0d3eb3912703f33a7cd62b0001f9
liaqat-programmer <liaqat122012@gmail.com>
2024-01-23 18:37:26 +0500

    Permutation

---
+++ Recursion/Permutation.java
@@ -0,0 +1,24 @@
+/**
+ * Permutation
+ */
+public class Permutation {
+
+    public static void PermutationOfElement(String str, String permutation){
+        if (str.length() == 0) {
+            System.out.println(permutation);
+            return;
+        }
+
+        for(int i=0; i<str.length(); i++){
+            char curr = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            PermutationOfElement(newStr, permutation+curr);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+
+        PermutationOfElement(str, "");
+    }
+}
