a75659825f3ff4ced6c6bf38e934c8a554bc472a
liaqat-programmer <liaqat122012@gmail.com>
2024-01-23 18:55:55 +0500

    Find Total Paths

---
+++ Recursion/TotalPaths.java
@@ -0,0 +1,28 @@
+/**
+ * TotalPaths
+ */
+public class TotalPaths {
+
+    public static int Totolpahts(int i, int j, int m, int n){
+        if(i == m || j == n){
+            return 0;
+        }
+        if (i == m-1 && j == n-1) {
+            return 1;
+        }
+
+        int DownPaths = Totolpahts(i+1,j,m,n);
+        int RightPaths = Totolpahts(i,j+1,m,n);
+
+        return DownPaths + RightPaths;
+    }
+
+    public static void main(String[] args) {
+        int m = 3;
+        int n = 3;
+
+       int paths = Totolpahts(0, 0, m, n);
+
+       System.out.println(paths);
+    }
+}
